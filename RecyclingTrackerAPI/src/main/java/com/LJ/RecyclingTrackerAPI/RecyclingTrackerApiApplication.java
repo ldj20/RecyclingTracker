0f7224715f6ea31446de7f85af6353e4fcb90bd6
ldj20 <lawrencedjiang@gmail.com>
2020-07-31 16:47:01 -0400

    change permitAll to include HttpMethod.OPTIONS

--- RecyclingTrackerAPI/src/main/java/com/LJ/RecyclingTrackerAPI/RecyclingTrackerApiApplication.java
+++ RecyclingTrackerAPI/src/main/java/com/LJ/RecyclingTrackerAPI/RecyclingTrackerApiApplication.java
@@ -14,7 +14,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication()
 public class RecyclingTrackerApiApplication /*implements CommandLineRunner*/ {
 
 	@Autowired
--- RecyclingTrackerAPI/src/main/java/com/RecyclingTrackerAPI/config/WebSecurityConfig.java
+++ RecyclingTrackerAPI/src/main/java/com/RecyclingTrackerAPI/config/WebSecurityConfig.java
@@ -16,22 +16,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	http
-    		.headers().frameOptions().disable()
-    		.and()
-    		.csrf()
-	    	.disable()
+    		.csrf().disable()
 	    	.authorizeRequests()
+	    	.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+	    	/*
 	        .antMatchers(HttpMethod.POST,"/users").permitAll()
 	        .antMatchers(HttpMethod.GET, "/users").permitAll()
 	        .antMatchers(HttpMethod.GET,"/users/*").permitAll()
 	        .antMatchers(HttpMethod.PUT,"/users/*").permitAll()
 	        .antMatchers(HttpMethod.DELETE,"/users").permitAll()
+	        */
 	        .anyRequest()
 	        .authenticated()
 	        .and()
-	        .httpBasic()
-    		.and().formLogin().permitAll();
-    	
+	        .httpBasic();
+    		//.and().formLogin().permitAll();
     }
     
     @Bean
