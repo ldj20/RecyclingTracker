cc363638359b412dfe8cf57b5844782196efc38d
Lawrence Jiang <lawrence.jiang@duke.edu>
2020-08-18 21:48:21 -0400

    add vision API

---
+++ RecyclingTrackerAPI/src/main/java/com/LJ/RecyclingTrackerAPI/controller/ServicesController.java
@@ -0,0 +1,82 @@
+package com.LJ.RecyclingTrackerAPI.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.LJ.RecyclingTrackerAPI.domain.DisplayData;
+import com.LJ.RecyclingTrackerAPI.domain.UserAccount;
+import com.LJ.RecyclingTrackerAPI.service.TrackerService;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature.Type;
+
+@RestController
+@RequestMapping("/services")
+public class ServicesController {
+	
+	@Autowired
+	private TrackerService trackerService;
+	@Autowired 
+	private CloudVisionTemplate cloudVisionTemplate;
+	@Autowired private 
+	ResourceLoader resourceLoader;
+
+	@GetMapping("/extractLabels")
+	public ModelAndView extractLabels(String imageUrl, ModelMap map) {
+		AnnotateImageResponse response =
+		        this.cloudVisionTemplate.analyzeImage(
+		            this.resourceLoader.getResource(imageUrl), Type.LABEL_DETECTION);
+
+		    Map<String, Float> imageLabels =
+		        response
+		            .getLabelAnnotationsList()
+		            .stream()
+		            .collect(
+		                Collectors.toMap(
+		                    EntityAnnotation::getDescription,
+		                    EntityAnnotation::getScore,
+		                    (u, v) -> {
+		                      throw new IllegalStateException(String.format("Duplicate key %s", u));
+		                    },
+		                    LinkedHashMap::new));
+		    // [END spring_vision_image_labelling]
+
+		    map.addAttribute("annotations", imageLabels);
+		    map.addAttribute("imageUrl", imageUrl);
+
+		    return new ModelAndView("result", map);
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getDisplayData(@PathVariable("id") String id) {
+		UserAccount result = trackerService.findById(id);
+		
+		//uncomment below line and comment above line to test various different values for UserAccount
+		//UserAccount result = testUser;
+		
+		String message = "You still have some habits to build up, but you'll get there eventually!";
+		Boolean background = false;
+		if (result.getPoints() > 0) {
+			message = "You've been recycling well and reaching your goal! Keep it up!";
+			background = true;
+		}
+		
+		DisplayData returnData = new DisplayData(result.getFrequency(), message, background, result.getPoints());
+		
+		return new ResponseEntity(returnData, HttpStatus.OK);
+	}
+	
+	
+}
